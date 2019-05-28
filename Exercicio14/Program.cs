585c186737c0732e1e4d748b642f0b27b0d13947
Alberto <albertomaronezio@hotmai.com>
2019-05-28 17:08:17 -0300

    Exercicio14

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double PR1, PR2 = 0;
+            double media = 0;
+            Console.Write("Digite a nota da prova 1: ");
+            PR1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite a nota da prova 2: ");
+            PR2 = double.Parse(Console.ReadLine());
+            media = (PR1 + PR2) / 2;
+            Console.WriteLine("A Média do Aluno é: {0}", media);
+        }
+    }
+}
